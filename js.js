00b5e9d942040f16fdaefb3cf6a2518f0fe97350
Gabo <beliczky3@gmail.com>
2025-03-12 10:34:16 +0100

    StuffLoad1

---
+++ js.js
@@ -0,0 +1,49 @@
+class Question
+{
+    constructor(kerdes,valasz1,pont1,valasz2,pont2,valasz3,pont3)
+    {
+        this.kerdes = kerdes
+        this.valasz1 = valasz1
+        this.pont1 = pont1
+        this.valasz2 = valasz2
+        this.pont2 = pont2
+        this.valasz3 = valasz3
+        this.pont3 = pont3
+    }
+}
+let Questions = []
+let QuestionIndex = 0
+let Points = 0
+let values = [1,2,3]
+const endResults = 
+[
+    "13 - 21 Gratulálunk, te tudod, hogy kell igazán egészségesen élni. Ami nagyon fontos, hogy továbbra is figyelj oda a megfelelő hidratálásra és a rostbevitelre. Ha még nem próbáltad, akkor itt az ideje kipróbálni az alternatív fehérje megoldásokat is. Szuper egészséges és finom tud lenni. Egyre vigyázz, azért ne hajtsd túl magad. ;)",
+    "22 - 30 Jó úton jársz, de még van mit javítani az étkezéseden. Figyelj a rost és a megfelelő fehérje bevitelre (hal, pulyka vagy csirke legyen a fő és a hüvelyes zöldségek). Nézz utána a mediterrán étrendnek, a tested meg fogja hálálni. A nassolást, amennyire lehet, mellőzd. A nyugodt alváshoz pedig elengedhetetlen a jó környezet, a sötét szoba. Nyugi, nincs szörny az ágy alatt. ;)",
+    "31 - 39 Ajjaj, nagy a baj. Nem figyelsz az étkezésedre. Ha ezen nem változtatsz, komoly egészségügyi következményei is lehetnek (mint a cukorbetegség, a magas vérnyomás vagy a korai csontritkulás). Légy tudatos, egy életünk van. Javasoljuk, hogy a gyorsan felszívódó szénhidrátokat (vagy épp a szupergyorsan felszívódókat) -mint a nassok, sütemények, krumpli, rizs- cseréld lassan felszívódó szénhidrátokra – basmati rizs, hajdina, köles, kuszkusz- és fogyassz elég folyadékot. Minden nap legalább egy 4km-es távot sétálj le gyorssétával. Ha azt érzed, hogy nehézkes az alvás, akkor lefekvés előtt egy 30 perccel már ne nézz tv-t és ne használd a telefonodat sem. Így nyugodtabb lesz az alvásod és másnap nem kelsz fáradtan, ami miatt amúgy összezabálsz mindent."
+]
+function InitializeQuestion(index)
+{
+    if (index < Questions.length)
+    {
+        let Element = document.getElementById("QuestionForm")
+        values[0] = Questions[index].pont1
+        values[1] = Questions[index].pont2
+        values[2] = Questions[index].pont3
+        Element.innerHTML = 
+        `
+                <h1>${Questions[index].kerdes}</h1>
+                <input type="radio" name="Kerdes" id="Kerdes" value="1"><label for="Kerdes">${Questions[index].valasz1}</label><br>
+                <input type="radio" name="Kerdes" id="Kerdes" value="2"><label for="Kerdes">${Questions[index].valasz2}</label><br>
+                <input type="radio" name="Kerdes" id="Kerdes" value="3"><label for="Kerdes">${Questions[index].valasz3}</label><br>
+                <button type="button" onclick="NextQuestion()">Következö Kérdés</button>
+        `
+    }
+}
+fetch("kerdesek.json").then(JSON =>JSON.json()).then(file => 
+{
+    file.forEach(element => 
+    {
+        Questions.push(new Question(element.kerdes,element.valasz1,element.pont1,element.valasz2,element.pont2,element.valasz3,element.pont3))
+    });
+    InitializeQuestion(QuestionIndex)
+})
